2c976cd21146158c03e8f027052f570816faaccf
Campos <tiago.a.campos@accenture.com>
2025-01-04 21:25:06 -0300

    components createds with styles

---
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AppComponent } from '../../app.component';
+
+@Component({
+  selector: 'app-menu-bar',
+  imports: [],
+  templateUrl: './menu-bar.component.html',
+  styleUrl: './menu-bar.component.css'
+})
+export class MenuBarComponent {
+
+}
---
+++ src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { AppComponent } from '../../app.component';
+
+@Component({
+  selector: 'app-menu-title',
+  imports: [],
+  templateUrl: './menu-title.component.html',
+  styleUrl: './menu-title.component.css'
+})
+export class MenuTitleComponent {
+
+}
